ca43faa139331caae5a56cbe33ab77b4d7f23e55
jryancopeland <45051209+jryancopeland@users.noreply.github.com>
2020-05-03 19:11:28 -0500

    Install files

---
+++ WindowsInstall.ps1
@@ -0,0 +1,17 @@
+pip install Flask
+pip install flask_login
+pip install datetime
+pip install flask_wtf
+pip install wtforms
+pip install wtforms.validators
+pip install sqlalchemy
+pip install config
+pip install flask_sqlalchemy
+pip install flask_migrate
+pip install flask_bootstrap
+pip install app
+pip install pymysql
+pip install werkzeug
+pip install cryptography
+clear
+Write-Host "Installation Complete"
